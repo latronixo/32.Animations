997619f48592c5699c5a334ec374749ffbc38f9b
Valentin <latronixo@gmail.com>
2025-05-30 20:14:53 +0300

    Демонстрация модификатора .scaleEffect

--- 32.Animations/ContentView.swift
+++ 32.Animations/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap me") {
+            print("кнопку тапнули")
+            animationAmount += 1
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
     }
 }
 
