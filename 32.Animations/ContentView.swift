664ba78f1ba50807f50a42174996733b34b1cb96
Valentin <latronixo@gmail.com>
2025-05-31 10:36:26 +0300

    Перемещаем градиент пальцем. При отпускании пальца градиент возвращается на первоначальное место

--- 32.Animations/ContentView.swift
+++ 32.Animations/ContentView.swift
@@ -8,18 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     var body: some View {
-        Button("Tap me") {
-            enabled.toggle()
-        }
-        .frame(width: 200, height: 200)
-        .animation(nil, value: enabled)
-        .foregroundStyle(.white)
-        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
-        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
-        .background(enabled ? .blue : .red)
-        
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in dragAmount = .zero }
+            )
     }
 }
 
